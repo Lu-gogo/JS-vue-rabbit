35249f2b411bb72dd0cbed88812e79d809325c77
Lu-gogo <XI-LANK@outlook.com>
2025-07-01 21:59:44 +0800

    axios_pack

---
+++ src/utils/http.js
@@ -0,0 +1,20 @@
+//axios basic pack
+import axios from 'axios';
+
+const httpInstance = axios.create({
+  baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
+  timeout: 5000
+})
+
+// Add a request interceptor
+// axios请求拦截器
+httpInstance.interceptors.request.use(config => {
+  return config
+}, e => Promise.reject(e))
+
+// axios响应式拦截器
+httpInstance.interceptors.response.use(res => res.data, e => {
+  return Promise.reject(e)
+})
+
+export default httpInstance;
