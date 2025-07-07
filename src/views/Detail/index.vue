82246544084a88be096caa670a5efe1a36f6be33
Lu-gogo <XI-LANK@outlook.com>
2025-07-07 17:12:02 +0800

    small active

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,7 +3,7 @@ import DetailHot from './components/DetailHot.vue'
 import { onMounted, ref } from 'vue';
 import { getDetailAPI } from '@/apis/detail';
 import { useRoute } from 'vue-router'
-
+import ImgView from '@/components/imgView/index.vue'
 
 
 const goods = ref({})
@@ -39,7 +39,7 @@ onMounted(()=>getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImgView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
