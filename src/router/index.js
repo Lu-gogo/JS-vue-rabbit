6f791b6ccbfcf57f742cacf835e7118534554e07
Lu-gogo <XI-LANK@outlook.com>
2025-07-09 23:10:25 +0800

    third router userIfo userOrder

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,10 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import userInfo from '@/views/Member/components/userInfo.vue'
+import userOrder from '@/views/Member/components/userOrder.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +55,20 @@ const router = createRouter({
           path: 'paycallback', // 注意路径，必须是paycallback
           component: PayBack
         },
+        {
+          path: '/member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: userInfo
+            },
+            {
+              path: 'order',
+              component: userOrder
+            }
+          ]
+        }
       ]
     },
     {
